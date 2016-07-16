0fcf14cf3784cb6033bae14ff245fe5f58cd240e
Jake Dawkins <dawkinsjh@gmail.com>
2016-07-16 11:11:23 -0400

    cleaned up template files and removed home dir

--- public_html/partials/bs_head.php
+++ public_html/partials/bs_head.php
@@ -1,23 +1,23 @@
 <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Jake Dawkins's. Developer: web, iOS.">
-    <meta name="author" content="Jake Dawkins">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Jake Dawkins's. Developer: web, iOS.">
+<meta name="author" content="Jake Dawkins">
 
-    <!-- Bootstrap Core CSS -->
-    <link href="<?php echo '/css/bootstrap.min.css' /*Path::css().'bootstrap.min.css'*/ ?>" rel="stylesheet">
+<!-- Bootstrap Core CSS -->
+<link href="<?php echo '/css/bootstrap.min.css' /*Path::css().'bootstrap.min.css'*/ ?>" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="<?php echo '/css/mainstyle.css' /*Path::css().'mainstyle.css'*/ ?>" rel="stylesheet">
+<!-- Custom CSS -->
+<link href="<?php echo '/css/mainstyle.css' /*Path::css().'mainstyle.css'*/ ?>" rel="stylesheet">
 
-    <!-- Custom Fonts -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+<!-- Custom Fonts -->
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
