8aac478d1ba2ceec49c94053e51d999c35dcc09a
Jake Dawkins <dawkinsjh@gmail.com>
2016-09-29 21:19:16 -0400

    moved over boilerplate

--- home/app/components/Contact.jsx
+++
@@ -1,9 +0,0 @@
-let React = require('react');
-
-const Contact = () => (
-  <div id="test-inside">
-    hello world
-  </div>
-);
-
-module.exports = Contact;
--- home/app/components/Photos.jsx
+++
@@ -1,9 +0,0 @@
-let React = require('react');
-
-const Photos = () => (
-  <div id="test-inside">
-    hello world
-  </div>
-);
-
-module.exports = Photos;
