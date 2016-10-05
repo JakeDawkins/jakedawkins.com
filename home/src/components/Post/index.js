bee753b465774cdc8ec794cf8a53eb8f8206d8ee
Jake Dawkins <dawkinsjh@gmail.com>
2016-10-04 22:03:06 -0400

    added back button and disabler to posts

--- home/src/components/Post/index.js
+++ home/src/components/Post/index.js
@@ -1,16 +1,28 @@
 import React, { PropTypes } from "react";
 import ReactMarkdown from "react-markdown";
+import { Link } from "react-router";
 import "../../styles/_post.scss";
 
 const Post = (props) =>
   <section className="post" itemScope itemType="http://schema.org/BlogPosting">
       <article role="article" id="scroll" className="post-content" itemProp="articleBody">
+        { props.disableBackLink ? null :
+          <p>
+            <Link to="/posts">&lt;&nbsp;&nbsp;Back to Posts</Link>
+          </p>
+        }
         <ReactMarkdown source={props.markdown} />
+        { props.disableBackLink ? null :
+          <p style={{textAlign: "center"}}>
+            <Link to="/posts">&lt;&nbsp;&nbsp;Back to Posts</Link>
+          </p>
+        }
       </article>
   </section>
 
 Post.propTypes = {
-  markdown: PropTypes.string.isRequired
+  markdown: PropTypes.string.isRequired,
+  disableBackLink: PropTypes.bool,
 }
 
 export default Post;
