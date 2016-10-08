ff636261ef7156933dd4255c19898084fb1d190a
Jake Dawkins <dawkinsjh@gmail.com>
2016-10-08 16:46:33 -0400

    added post header test

---
+++ home/src/components/PostHeader/index.test.js
@@ -0,0 +1,32 @@
+import PostHeader from './';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+describe("PostHeader", () => {
+
+  it("should exist", () => {
+    const postHeader = renderer.create(
+      <PostHeader />
+    );
+
+    expect(postHeader).toMatchSnapshot();
+  });
+
+  it("should render props correctly", () => {
+    const postHeader = renderer.create(
+      <PostHeader />
+    );
+
+    const postHeaderWithProps = renderer.create(
+      <PostHeader
+        dateTime="12345"
+        title="test title"
+        description="description here"
+      />
+    );
+
+    expect(postHeaderWithProps).toMatchSnapshot();
+    expect(postHeaderWithProps).not.toBe(postHeader);
+  });
+
+});
